bcdae4925348e086173f41479fe56c84869035f8
JennaFi <filimonova.evgeniya@gmail.com>
2021-02-21 02:16:11 +0300

    navigation

--- src/navigation/HomeTabNavigator.js
+++ src/navigation/HomeTabNavigator.js
@@ -7,6 +7,7 @@ import Feather from 'react-native-vector-icons/Feather';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 import HomeScreen from '../screens/HomeScreen';
+import ExploreNavigator from './ExploreNavigator';
 
 const Tab = createBottomTabNavigator();
 const HomeTabNavigator = () => {
@@ -17,7 +18,7 @@ const HomeTabNavigator = () => {
       }}>
       <Tab.Screen
         name={'Explore'}
-        component={HomeScreen}
+        component={ExploreNavigator}
         options={{
           tabBarIcon: ({color}) => (
             <Fontisto name="search" size={25} color={color} />
